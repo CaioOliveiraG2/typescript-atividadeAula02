eee5fa3e80b6cc6a2ab16fc9f2490ec12b1dd014
CaioOliveiraG2 <caio.silva137@fatec.sp.gov.br>
2022-08-26 21:59:04 -0300

    Atividade-Aula02-Persona

--- index.ts
+++ index.ts
@@ -1,6 +1,12 @@
-// Import stylesheets
-import './style.css';
+import { Person } from './person.ts';
 
-// Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+let person = new Person('Caio', 'Oliveira', 'Silva', 18);
+
+const app: HTMLElement = document.getElementById('app');
+app.innerHTML = `
+<h1>Persona</h1>
+<h2>Me chamo ${person.getFullName()}</h2>
+<h2>Nasci em ${person.getBirthdayYear()}</h2>
+<h3>${person.emails[0]}</h3>
+<h3>${person.emails[1]}</h3>
+<h3>${person.emails[2]}</h3>`;
